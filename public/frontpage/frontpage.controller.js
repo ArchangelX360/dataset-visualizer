495c06ba4ad51f9ee9655116b9647d9982721f10
Titouan Bion <titouan.bion@gmail.com>
2016-06-20 16:09:05 +0200

    feature: index link on YCSB title too and some new texts

--- public/frontpage/frontpage.controller.js
+++ public/frontpage/frontpage.controller.js
@@ -1,5 +1,5 @@
 angular.module('frontpageController', [])
 
     .controller('FrontpageController', function ($scope, $rootScope) {
-        $rootScope.pageTitle = 'YCSB Visualisation';
+        $rootScope.pageTitle = 'What do you want to do ?';
     });
