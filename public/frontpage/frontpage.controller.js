0fe0c7b2ba37d619abb93bdf01bb6bd22c74d6cd
Titouan Bion <titouan.bion@gmail.com>
2016-07-13 18:09:46 +0200

    feature: presentation architecture scheme

--- public/frontpage/frontpage.controller.js
+++ public/frontpage/frontpage.controller.js
@@ -2,4 +2,16 @@ angular.module('frontpageController', [])
 
     .controller('FrontpageController', function ($scope, $rootScope) {
         $rootScope.pageTitle = 'What do you want to do ?';
+        $scope.presentation = true;
+
+        $scope.ycsb = false;
+        $scope.visualizer = false;
+
+        $scope.toggleYCSB = function () {
+            $scope.ycsb = !$scope.ycsb;
+        };
+
+        $scope.toggleVisualizerApp = function () {
+            $scope.visualizer = !$scope.visualizer;
+        };
     });
