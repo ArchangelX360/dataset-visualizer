cfba5291120aa485b190a0c8054de120255f7a09
Titouan BION <titouan.bion@gmail.com>
2016-10-11 15:44:56 +0200

    fix: remove presentation in controller

--- public/frontpage/frontpage.controller.js
+++ public/frontpage/frontpage.controller.js
@@ -2,21 +2,4 @@ angular.module('frontpageController', [])
 
     .controller('FrontpageController', function ($scope, $rootScope) {
         $rootScope.pageTitle = 'What do you want to do ?';
-        $scope.presentation = true;
-
-        $scope.software = false;
-        $scope.visualizer = false;
-        $scope.arrows = false;
-
-        $scope.toggleSoftware = function () {
-            $scope.software = !$scope.software;
-        };
-
-        $scope.toggleVisualizerApp = function () {
-            $scope.visualizer = !$scope.visualizer;
-        };
-
-        $scope.toggleArrows = function () {
-            $scope.arrows = !$scope.arrows;
-        };
     });
